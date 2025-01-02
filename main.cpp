9751777c8342548241129bf75d7b790f2944e5d9
tomwomwen <tomwomwen@163.com>
2025-01-02 11:55:05 +0800

    创建第一个main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void sort(int arr[],int size){
+	
+	
+}
+
+int main(){
+		int arr[] = {12,31,32,32};
+		int size = sizeof(arr)/sizeof(arr[0]);
+		sort(arr,size);
+		return 0;
+	
+	
+}
