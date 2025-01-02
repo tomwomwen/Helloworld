95605ff08a1fb9ef4953c508a216830c5dc673cf
tomwomwenlinux <tomwomwen@163.com>
2025-01-02 12:22:29 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
 void sort(int arr[],int size){
-	
+    int temp = 0;
+    for (int i=0;i<size-1;++i){
+		for(int j=0;j<size-1-j;++j){
+			if(arr[j] < arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+
+	}
 	
 }
 
